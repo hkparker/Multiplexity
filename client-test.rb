8c2eb0c37522ddd2d8408c722f86e7b87cd3acf5
Hayden Parker <haydenkparker@gmail.com>
2013-06-24 10:29:56 -0700

    working on routing setup stuff

--- client-test.rb
+++ client-test.rb
@@ -9,7 +9,27 @@ def get_ip
 	return "127.0.0.1"
 end
 
+def get_bool
+	choice = ""
+	until choice == "y" or choice == "n"
+		print ">"
+		choice = STDIN.gets.chomp
+	end
+	choice
+end
+
+def setup_routes
+	puts "Setting up source based routing".good
+end
+
 puts "Loading Multiplex client ".good
+
+puts "Before we connect to a server we need to setup routing rules".good
+puts "If you have already setup source based routing, you can skip this step".good
+puts "Would you like to setup routing rules now?".question
+choice = get_bool
+setup_routes if choice == "y"
+
 puts "Please enter the IP address of the multiplex server".question
 server = get_ip
 
