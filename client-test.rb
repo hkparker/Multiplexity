832978a54b6c5312a2ad1a395524abe7b12a173f
Hayden Parker <haydenkparker@gmail.com>
2013-06-24 11:40:35 -0700

    working on menu for setting up routing rules

--- client-test.rb
+++ client-test.rb
@@ -1,12 +1,25 @@
 #!/usr/bin/env ruby
 
 require './multiplexity_client.rb'
+require './ippair.rb'
 require 'socket'
 
 port = 8000
 
+def is_ip?(address)	# from IPAddr standard library
+	if /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\Z/ =~ address
+		return $~.captures.all? {|i| i.to_i < 256}
+	end
+	return false
+end
+
 def get_ip
-	return "127.0.0.1"
+	address = ""
+	until is_ip?(address)
+		print ">"
+		address = STDIN.gets.chomp
+	end
+	address
 end
 
 def get_bool
@@ -18,8 +31,45 @@ def get_bool
 	choice
 end
 
+def is_digit?(string)
+	begin
+		Float(string)
+	rescue
+		false
+	else
+		true
+	end
+end
+
+def get_int
+	int = ""
+	until is_digit?(int) == true
+		print ">"
+		int = STDIN.gets.chomp
+	end
+	int.to_i
+end
+
 def setup_routes
 	puts "Setting up source based routing".good
+	puts "Please join all networks you plan to use now".good
+	puts "Press enter once you have an IP on each network".good
+	STDIN.gets
+	puts "How many network interfaces would you like to multiplex across?".question
+	route_list = []
+	get_int.times do |i|
+		puts "Please enter the IP address of the interface"
+		address = get_ip
+		puts "Please enter the corresponding default gateway for that interface"
+		gateway = get_ip
+		route_list << IPPair.new(address, gateway)
+	end
+	puts "To confirm:".good
+	route_list.each do |pair|
+		puts "IP Address #{pair.address} uses gateway #{pair.gateway}"
+	end
+	
+	exit 0
 end
 
 puts "Loading Multiplex client ".good
@@ -27,8 +77,7 @@ puts "Loading Multiplex client ".good
 puts "Before we connect to a server we need to setup routing rules".good
 puts "If you have already setup source based routing, you can skip this step".good
 puts "Would you like to setup routing rules now?".question
-choice = get_bool
-setup_routes if choice == "y"
+setup_routes if get_bool == "y"
 
 puts "Please enter the IP address of the multiplex server".question
 server = get_ip
@@ -42,8 +91,6 @@ rescue
 end
 
 client = MultiplexityClient.new(socket)
-
-# get information on interfaces, setup routing rules
 
 client.handshake
 client.setup_multiplex
---
+++ ippair.rb
@@ -0,0 +1,14 @@
+class IPPair
+	def initialize(address, gateway)
+		@address = address
+		@gateway = gateway
+	end
+	
+	def address
+		@address
+	end
+	
+	def gateway
+		@gateway
+	end
+end
